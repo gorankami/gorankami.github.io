c526c2aa743a0a07f076b294709ab81db7fab580
goran <gorankami@gmail.com>
2021-02-08 18:20:15 +0100

    Added script for creating microblogs

--- new.sh
+++ new.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 DATE="$(date +%F)"
-FILENAME="_posts/microblog/$DATE-$1"
-touch $FILENAME
-echo $2 >> $FILENAME
+FILENAME="_posts/microblog/$DATE-$1.md"
+
+if [[ -f $FILENAME ]];then
+    echo "$FILENAME exists"
+else
+    touch $FILENAME
+    echo '---' >> $FILENAME
+    echo "date: $DATE" >> $FILENAME
+    echo -e 'tags:\ncategory: microblog\nexcerpt_separator: <!--more-->\n---\n' >> $FILENAME
+    echo $2 >> $FILENAME
+    echo -e '\n<!--more-->' >> $FILENAME
+fi
