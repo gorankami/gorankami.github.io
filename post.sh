2802a6897289ff9c206b532c609e38049508638d
goran <gorankami@gmail.com>
2021-03-06 19:04:44 +0100

    Added scripts for post and journal

---
+++ post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE="$(date +%F)"
+FILENAME="_posts/$DATE-$1.md"
+
+if [[ -f $FILENAME ]];then
+    echo "$FILENAME exists"
+else
+    touch $FILENAME
+    echo '---' >> $FILENAME
+    echo "date: $DATE" >> $FILENAME
+    echo -e 'tags:\ncategory: \nexcerpt_separator: <!--more-->\n---\n' >> $FILENAME
+    echo $2 >> $FILENAME
+    echo -e '\n<!--more-->' >> $FILENAME
+fi
