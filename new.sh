1286fa8c6e0535882d6bb04ff46296e354d01493
Goran Antic <goran.antic@evermedtv.com>
2021-12-02 12:14:26 +0100

    added "_new" dir for starting new blogs, _draft is now in git

--- new.sh
+++ new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE="$(date +%F)"
-FILENAME="_posts/microblog/$DATE-$1.md"
+FILENAME="_new/$DATE-$1.md"
 
 if [[ -f $FILENAME ]];then
     echo "$FILENAME exists"
@@ -9,7 +9,7 @@ else
     touch $FILENAME
     echo '---' >> $FILENAME
     echo "date: $DATE" >> $FILENAME
-    echo -e 'tags:\ncategory: microblog\nexcerpt_separator: <!--more-->\n---\n' >> $FILENAME
+    echo -e 'tags:\ncategory: \nexcerpt_separator: <!--more-->\n---\n' >> $FILENAME
     echo $2 >> $FILENAME
     echo -e '\n<!--more-->' >> $FILENAME
 fi
